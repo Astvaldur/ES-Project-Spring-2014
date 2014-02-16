b3ca55829b96486434b1cf6b6a4b258dd7bdfca6
David Alm <goliat.90@gmail.com>
2014-02-16 23:14:04 +0100

    Worked on the settings for Doxygen VHDL code generation

--- DoxyGen/vhdl_template.vhd
+++ DoxyGen/vhdl_template.vhd
@@ -1,23 +1,22 @@
 -------------------------------------------------------
---! @file What is the name of the source file?
---! @brief short description. will end when a blank line or another sectioning command is parsed.
---! Give a description about the file
+--! @file vhdl_template.vhd
+--! @brief short description of the code on this line.
+--! @details Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+--! Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+--! when an unknown printer took a galley of type and scrambled it to make a type specimen book
 --! \author who created the this document. add another author command if your are several authors
 --! \version what version is the code in.
---! \vhdlflow[flowchart title] this command should create a flow chart.
 -------------------------------------------------------
  
---! Use standard library
+--!Use standard library
 library ieee;
---! Use logic elements
+--!Use logic elements
 use ieee.std_logic_1164.all;
 
---! Mux entity brief description written after Use statement
-
--- Detailed description of this
--- mux design element. Placed just above entity
-
-entity mux_using_with is
+--! I motsättning till vad många tror, är inte Lorem Ipsum slumvisa ord. 
+--! Det har sina rötter i ett stycke klassiskt litteratur på latin från 45 år före år 0, 
+--! och är alltså över 2000 år gammalt.
+entity vhdl_template_entity is
      port (
           din_0   : in  std_logic; 	--! Mux first input written next to the port
           din_1   : in  std_logic; 	--! Mux Second input written next to the port
@@ -26,9 +25,14 @@ entity mux_using_with is
      );
 end entity;
 
---! @brief brief description here about the architechture
---! @details Detailed description about the architecture of the mux.
-architecture behavior of mux_using_with is
+
+--! @details Det er en kendsgerning, at man bliver distraheret af 
+--! læsbart indhold på en side, når man betragter dens layout. 
+--! Meningen med at bruge Lorem Ipsum er, at teksten indeholder 
+--! mere eller mindre almindelig tekstopbygning i modsætning til 
+--! "Tekst her – og mere tekst her", mens det samtidigt ligner almindelig tekst. 
+--! Mange layoutprogrammer og webdesignere bruger Lorem Ipsum som fyldtekst.
+architecture architecture_of_vhdl_template of vhdl_template_entity is
 begin
           	with (sel) select
                mux_out <= din_0 when '0',
