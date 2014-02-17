1a24c705517e702ac40f47048302d785a388e097
Astvaldur <astvaldur89@gmail.com>
2014-02-17 23:21:16 +0100

    First version of PWM ready, proper FSM was the bug killer

--- Source code/DAC/PWM_interface.vhdl
+++ Source code/DAC/PWM_interface.vhdl
@@ -1,84 +1,175 @@
+-------------------------------------------------------
+--! @file source PWM_interface.vhdl
+--! @This is the Pwm. It ouputs pulses with Pulse width modulation
+--! to a fourth order Low pass filter where the pulses are filtered out.
+--! This code is developed using a 2 process model.
+
+--! \Astvaldur Hjartarson
+--! \1.0v
+--! \vhdlflow[PWM]
+-------------------------------------------------------
+
 ----------------------------------------------------------
 ------              PWM Control                    -------
 ----------------------------------------------------------
+--! Use standard library
 library IEEE;
+USE work.all;
+--! PWM uses standard logic elements
 use IEEE.std_logic_1164.all;
+--! Numeric standard.
 use ieee.numeric_std.all;
-use IEEE.MATH_REAL.ALL;
+--use IEEE.MATH_REAL.ALL;
 
-
+--! This is the PWM entity.
+--! 
 entity PWM is
    GENERIC(
-        width   : integer  := 12; -- bit resulution
-        op_freq : integer  := 200_000;
-        sys_clk : integer  := 100_000_000);
+        width   : integer  := 8; -- Bit resulution of the input bit vector
+        op_freq : integer  := 200_000; --! The 2*frequency of the Pulses that we output.
+        sys_clk : integer  := 100_000_000); --! The overall system clock
     PORT(
-    -- in
-	vsample: in	std_logic_vector(width-1 downto 0); -- Sample
-    --system
-	reset : in std_logic;    -- system reset
-    clk   : in std_logic;    -- System clock
-    -- out
-	ampPWM	: out	std_logic;  -- PWM signal 
-    ampSD	: out	std_logic   -- select the amplifier
+    --!system
+	reset : in std_logic;    --! System reset, active low.
+    clk   : in std_logic;    --! The clock input.
+    -- !in
+	vsample: in	std_logic_vector(width-1 downto 0); --! The PWM input signal.
+    -- !out
+	ampPWM	: out	std_logic;  --! The ouput PWM signal, It is connected to the input of the filter. 
+    ampSD	: out	 std_logic_vector(8 downto 0)   --! We need to select the filter to be on.
     );
 end entity;
 
-
 architecture RTL of PWM is
--- This is the current 
-CONSTANT pwm_period: INTEGER:=(sys_clk/op_freq)-1;
+-- This is the current Period can be set shorter for more interpolation.
 
+--! Function------------------------------------------------------------------
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+--! Function name: integerbitsize
+--! Before: The function takes a integer x as a input.
+--! After: We get return a integer that represent lowest amount of bits
+--!        needed to represent integer x.
+function integerbitsize(
+size : Integer)
+return integer is variable value : integer;
+variable tmp: integer;
+variable remainder: integer;
+begin
+    value:=0;
+    remainder:=size;
+    if(size<=0 or size=1) then
+        return 0;
+    elsif(abs(size)>1) then
+        while (remainder>0)  loop
+            remainder:=(remainder-(remainder mod 2))/2;
+            value:=value+1;
+        end loop;
+        return value;
+    end if;
+
+end  integerbitsize;
+--!------------------------------------------------------------------
+--!------------------------------------------------------------------
+
+--! Constants
+--! pwm_period is compiler constant for calulating the PWM period.
+CONSTANT pwm_period: INTEGER:=(sys_clk/op_freq)-1; --499
+--! Resulution is a constant for the highest resulution we can get for 
+--! the input bits.
+CONSTANT resolution: INTEGER:=2**(width);
+--! pwm_period is to see of large a counter we need. This might be change to 
+--! 32 bits or any other constant depending on how large the counter might ever
+--! become.
+CONSTANT Period_counter_range: INTEGER:= integerbitsize(pwm_period); 
+
+
+  type state_t is (output_high, output_low, new_data);
+
+--! The Record for the 2 process method.
 Type reg_t is record
-        pwm_out : std_logic ;
-        pwm_in : std_logic_vector(width-1 downto 0);
-        period_counter : std_logic_vector(width-1 downto 0);
-        pwm_index_shift : std_logic_vector(width-1 downto 0);
+        state : state_t;
+        pwm_out : std_logic ; --! Output for PWM 
+        pwm_in : std_logic_vector(width-1 downto 0);--! Input for PWM
+        period_counter : integer;--! Counter for period.
+        pwm_index_shift : integer;--! Index shift.
 end record;
 
--- registers
+--! registers r and rin for 2 process model.
 signal r, rin :reg_t;
 
 begin
-reg:process(clk,reset) -- sequential process
+--! sequential process
+reg:process(clk,reset) 
 begin
-    if(reset = '0') then -- 1 or 0 ?
+    if(reset = '0') then -- Active low
         r.pwm_out <= '0';
+        r.period_counter <= 0;
+        r.pwm_index_shift <=0;
         r.pwm_in  <= (others=>'0');
-        r.period_counter <= (others=>'0');
-        r.pwm_index_shift <= (others=>'0');
-      
-    elsif (rising_edge(CLK)) then -- COUNTER
+        r.state <= new_data;
+    elsif(rising_edge(clk)) then -- COUNTER    
         r<=rin;
-        r.period_counter <= std_logic_vector(unsigned(r.period_counter)+1);
     end if;
 end process;
 
-comb:process(r,vsample) -- combinatorial process
+
+--! combinatorial process --------------------------------------
+comb:process(r) 
 variable v : reg_t;
-begin
-     v:=r;
-      if(TO_INTEGER(UNSIGNED(r.period_counter)) = 0) then
+begin 
+
+    v:=r;
+    case r.state is
+    when new_data =>--(TO_INTEGER(UNSIGNED(r.period_counter)) = 0 ) =>
+          v.period_counter :=0;             
           -- new cycle started. collect sample
           v.pwm_in:=vsample; -- New Input
-          -- CALCULATE SHIFTING INDEX HERE
-          v.pwm_index_shift := STD_LOGIC_VECTOR(UNSIGNED(TO_INTEGER(UNSIGNED(v.pwm_in))*pwm_period/(2**width)),width);
-          --v.period_counter:=(others=>'0');
-      elsif(r.pwm_index_shift >= r.period_counter) then
-          -- if index number is larger or equal than counter keep it high
-          v.pwm_out := '1';
-             
-      elsif(r.period_counter > r.pwm_index_shift) then
-          -- if  the counter is larger than the index where the shift should occur. set pwm low
-          v.pwm_out := '0';
-      ELSIF(TO_INTEGER(UNSIGNED(r.period_counter)) = pwm_period) THEN
-        -- end of cycle reset index.
-        v.period_counter := (OTHERS => '0');
-      END IF;
- rin <= v;
-end process;
+          -- CALCULATE SHIFTING INDEX 
+          v.pwm_index_shift :=  To_integer(unsigned(vsample))*(pwm_period+1)/resolution;
+          IF(v.pwm_index_shift>1) THEN
+            v.state := output_high;
+            v.pwm_out := '1';
+          elsif(v.pwm_index_shift=1)THEN
+            v.state := output_low;
+            v.pwm_out := '1';
+          ELSE
+            v.state := output_low;
+            v.pwm_out := '0';
+          END IF;
+          
+      -- If index number is larger or equal than counter keep it high
+    when output_high =>--(r.pwm_index_shift >= r.period_counter) =>
+        v.period_counter := r.period_counter + 1;
+        v.pwm_out := '1';
+        if(r.pwm_index_shift = v.period_counter) then
+            v.pwm_out := '0';
+            v.state:=output_low;
+         elsif(pwm_period = v.period_counter ) then
+            v.state:=new_data;
+        end if;
+        
+        
+      -- If  the counter is larger than the index where the shift should occur. set pwm low      
+    when output_low =>--(r.period_counter > r.pwm_index_shift) =>
+        v.period_counter := r.period_counter+1;
+        v.pwm_out := '0';
+        if(pwm_period = v.period_counter) then
+            v.state:=new_data;
+        end if;
 
--- Pins for the modules
+    when others =>
+        v.pwm_out := '0';
+        v.period_counter := 0;
+        v.pwm_index_shift := 0;
+        v.pwm_in  := (others=>'0');
+        v.state := new_data;
+    end case;
+      rin <= v;
+      
+end process;
+-- Pins for the modules --------------------------------------
+
 ampPWM <= r.pwm_out;-- Input to LP
-ampSD <= '1';     -- This pin selects the amplifiers to be ON.
+ampSD <= std_logic_vector(To_unsigned(r.period_counter,9));     -- This pin selects the amplifiers to be ON.
 end RTL;
