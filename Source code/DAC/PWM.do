8dd6cbe5ec53fa35d9da6edc023bb0b478cf0609
David Alm <goliat.90@gmail.com>
2014-02-17 10:45:10 +0100

    latest pwm interface version & new pwm.do file

--- Source code/DAC/PWM.do
+++ Source code/DAC/PWM.do
@@ -1,4 +1,4 @@
--- ALU_do
+-- 
 config wave -signalnamewidth 1
 restart -f -nowave
 
@@ -6,6 +6,10 @@ view signals wave
 #add wave /*
 add wave -color brown tb_clk
 add wave -color blue tb_reset
-add wave -color yellow tb_vsample
-add wave -color brown tb_pwm
+add wave -color yellow -unsigned tb_vsample
+add wave -color "light yellow" -unsigned tb_sample_nr
+add wave -color "light blue"  -unsigned tb_index
+add wave -color violet -unsigned tb_pwm_change
+add wave -color "light green" tb_pwm_signal
+add wave -color green tb_pwm
 run 150000000 ns
--- Source code/DAC/PWM_interface.vhdl
+++ Source code/DAC/PWM_interface.vhdl
@@ -27,12 +27,13 @@ end entity;
 
 architecture RTL of PWM is
 -- This is the current 
-CONSTANT pwm_period: std_logic_vector:=std_logic_vector(to_unsigned(sys_clk/op_freq -1,width));
+CONSTANT pwm_period: INTEGER:=(sys_clk/op_freq)-1;
 
 Type reg_t is record
         pwm_out : std_logic ;
         pwm_in : std_logic_vector(width-1 downto 0);
-        period_counter : std_logic_vector(width-1 downto 0) ;
+        period_counter : std_logic_vector(width-1 downto 0);
+        pwm_index_shift : std_logic_vector(width-1 downto 0);
 end record;
 
 -- registers
@@ -41,14 +42,15 @@ signal r, rin :reg_t;
 begin
 reg:process(clk,reset) -- sequential process
 begin
-    if(reset = '1') then -- 1 or 0 ?
+    if(reset = '0') then -- 1 or 0 ?
         r.pwm_out <= '0';
         r.pwm_in  <= (others=>'0');
         r.period_counter <= (others=>'0');
+        r.pwm_index_shift <= (others=>'0');
       
     elsif (rising_edge(CLK)) then -- COUNTER
         r<=rin;
-        r.period_counter := std_logic_vector(unsigned(r.period_counter)+1);
+        r.period_counter <= std_logic_vector(unsigned(r.period_counter)+1);
     end if;
 end process;
 
@@ -56,21 +58,27 @@ comb:process(r,vsample) -- combinatorial process
 variable v : reg_t;
 begin
      v:=r;
-    
-      if(r.period_counter = pwm_period) then
-            v.pwm_in:=vsample; -- New Input
-            v.period_counter:=(others=>'0');
-      elsif(r.pwm_in >= r.period_counter) then
-             v.pwm_out := '1';  
-      elsif(r.period_counter > r.pwm_in) then
-             v.pwm_out := '0';
+      if(TO_INTEGER(UNSIGNED(r.period_counter)) = 0) then
+          -- new cycle started. collect sample
+          v.pwm_in:=vsample; -- New Input
+          -- CALCULATE SHIFTING INDEX HERE
+          v.pwm_index_shift := STD_LOGIC_VECTOR(UNSIGNED(TO_INTEGER(UNSIGNED(v.pwm_in))*pwm_period/(2**width)),width);
+          --v.period_counter:=(others=>'0');
+      elsif(r.pwm_index_shift >= r.period_counter) then
+          -- if index number is larger or equal than counter keep it high
+          v.pwm_out := '1';
+             
+      elsif(r.period_counter > r.pwm_index_shift) then
+          -- if  the counter is larger than the index where the shift should occur. set pwm low
+          v.pwm_out := '0';
+      ELSIF(TO_INTEGER(UNSIGNED(r.period_counter)) = pwm_period) THEN
+        -- end of cycle reset index.
+        v.period_counter := (OTHERS => '0');
       END IF;
  rin <= v;
 end process;
 
-
 -- Pins for the modules
 ampPWM <= r.pwm_out;-- Input to LP
 ampSD <= '1';     -- This pin selects the amplifiers to be ON.
-
 end RTL;
