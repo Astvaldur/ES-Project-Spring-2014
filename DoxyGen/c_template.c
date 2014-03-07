cc42b4cacd341d8a1fd689c9bafab48ace9447c8
David Alm <goliat.90@gmail.com>
2014-03-07 10:36:38 +0100

    updated vhdl-template, created a c-code-template.

---
+++ DoxyGen/c_template.c
@@ -0,0 +1,41 @@
+// you need to have "/** as the start of the comment block. "/*" will not be parsed!
+/**
+* @file c_template.c
+* @brief short description of the code on this line. 
+* @details Lorem Ipsum is simply dummy text of the printing and typesetting industry.    
+* the details section can be several lines compared to brief which should be one.
+* if several authors made the file just add another author command.
+* @author David Alm
+* @author David Victor Alm
+* @version 2.0
+*/ 
+
+
+#include <stdio.h>
+
+
+/**
+* This is the description of the main function.
+* It is the beginning of the program.
+* You always need a main ive heard or it won't compile.
+* @return Returns a Integer of value 0.
+*/
+int main()
+{
+  printf("Hello world\n");
+  return 0;
+}
+
+/**
+* The herp function is used as a example for parameters.
+* the "in" and "out" option under parameters is optional but i just wanted to show it exists.
+* @param  [in]   a   I just like sending Integers.
+* @param  [out] b   Doubles are nice as well.
+* @param  [in,out] string     But I like strings the most.
+* @return this is a void so nothing is returned.
+*/
+void herp(int a, double b, char string [256]){
+    int d;
+    d = 1337; 
+    
+}
