51f7c42fbf132856ece11bb96b8f9cd3d9e5e916
Zottyx <zotty@zotty.se>
2014-03-28 10:48:20 +0100

    Added Matlab LP-filter

---
+++ Source code/SIM/Matlab/LP.m
@@ -0,0 +1,30 @@
+%%Read audio vectors
+%[sine_y,sine_Fs] = audioread('tone24bit.flac');
+%[flip_y,flip_Fs] = audioread('Flip.mp3');
+[rock_y,rock_Fs] = audioread('Rockin.mp3');
+
+disp('File reads OK!')
+
+%%
+filtered_y = rock_y;
+
+coeffs = fir1(15, 1/44100,'low');
+
+for idx = 50001:(numel(rock_y)/3)
+    
+    tmp = 0;
+    
+    for n = 1:numel(coeffs)
+    
+        tmp = tmp + (coeffs(n) * rock_y(idx-n));
+    
+    end 
+    
+    filtered_y(idx) = tmp; 
+end
+
+disp('Data calculated!')
+
+audiowrite('LP_mono.wav',filtered_y,rock_Fs,'BitsPerSample',24,'Comment','This is my new audio file.');
+
+disp('Output written!')
