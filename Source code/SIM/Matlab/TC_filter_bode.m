c7416b98046d3c4e308d2a6f7f989925f73bded6
Zottyx <zotty@zotty.se>
2014-05-13 15:09:14 +0200

    Made updates to TC sim script and added script for sine wave generation

--- Source code/SIM/Matlab/TC_filter_bode.m
+++ Source code/SIM/Matlab/TC_filter_bode.m
@@ -3,22 +3,27 @@ close all;
 clc;
 
 lp_order = 2;
-bp_order = 3;
+bp_order = 2;
 hp_order = 2;
 
-cutoff_low = 300;
-cutoff_high = 2400;
+LP_cutoff = 300;
+
+BP_cutoff_low = 300;
+BP_cutoff_hi = 2400;
+
+HP_cutoff = 2400;
 Fs = 48000;
 
 
-[X_lp, Y_lp] = butter(lp_order, cutoff_low/Fs, 'low');
-[X_bp, Y_bp] = butter(bp_order, [cutoff_low/Fs cutoff_high/Fs], 'bandpass');
-[X_hp, Y_hp] = butter(hp_order, cutoff_high/Fs, 'high');
+[X_lp, Y_lp] = butter(lp_order, LP_cutoff/Fs, 'low');
+[X_bp, Y_bp] = butter(bp_order, [BP_cutoff_low/Fs BP_cutoff_hi/Fs], 'bandpass');
+[X_hp, Y_hp] = butter(hp_order, HP_cutoff/Fs, 'high');
 
 sys_lp = tf(X_lp, Y_lp, 1/Fs);
 sys_bp = tf(X_bp, Y_bp, 1/Fs);
 sys_hp = tf(X_hp, Y_hp, 1/Fs);
 
+%bodeplot(sys_lp, sys_bp, Fs);
 
 hold on
 bode(sys_lp);
